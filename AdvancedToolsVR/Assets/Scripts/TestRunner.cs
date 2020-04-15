dc8c1b46df145211f88286516394b2f9cb8883bf
Steefman <svolkers99@gmail.com>
2020-04-15 13:39:07 +0200

    Added pictures for GitHub Readme and cleaned project

--- AdvancedToolsVR/Assets/Scripts/TestRunner.cs
+++ AdvancedToolsVR/Assets/Scripts/TestRunner.cs
@@ -10,11 +10,6 @@ public class TestRunner : MonoBehaviour
     [SerializeField] private PerformanceLogger _performanceLogger = null;
     [SerializeField] private TextMeshProUGUI _testRunningText = null;
 
-    [Header("Test information")]
-    [SerializeField] private string _currentRunningTest = "";
-    [SerializeField] private float secondsUntilTestComplete = 0;
-    [SerializeField] private bool _testRunning;
-
     public List<TestData> _testQueue = new List<TestData>();
     private List<TestData> _testResults = new List<TestData>();
 
@@ -28,7 +23,6 @@ public class TestRunner : MonoBehaviour
 
     private void Update()
     {
-        if (_testRunning) secondsUntilTestComplete -= Time.deltaTime;
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 300;
     }
@@ -43,6 +37,7 @@ public class TestRunner : MonoBehaviour
             TestData nextTest = _testQueue[0];
             StartCoroutine(runTest(nextTest));
             _testQueue.Remove(nextTest);
+
             _testRunningText.text = "<color=orange>Test Running - " + (_totalTests - _testQueue.Count) + " / " + _totalTests + "</color>";
             Debug.Log("<color=yellow>" + _testQueue.Count + " more tests in queue </color>");
             return;
@@ -59,26 +54,21 @@ public class TestRunner : MonoBehaviour
     {
         TestData testData = new TestData(pData);
         _objectSpawner.CleanObjects();
-        yield return null;
-        yield return new WaitForSeconds(1);
         Debug.Log("<color=yellow> Preparing new text... </color>");
 
+        yield return new WaitForSeconds(1);
         yield return new WaitUntil(() => _objectSpawner.GenerateObjectGrid(testData.gridSize, testData.type)); // Generate object grid
 
-        secondsUntilTestComplete = testData.testDurationSeconds;
-        _currentRunningTest = testData.testDescription;
         testData.objectCount = testData.gridSize.x * testData.gridSize.y * testData.gridSize.z;
         testData.trisCount = _objectSpawner.GetTrisCount();
 
-        yield return new WaitForSeconds(30); // Give it some extra time to load
+        yield return new WaitForSeconds(5); // Give it some extra time to load
         Debug.Log("<color=yellow> Next test: " + testData.testDescription +"</color>");
 
         // Recording test
-        _testRunning = true;
         _performanceLogger.StartRecording();
         yield return new WaitForSeconds(testData.testDurationSeconds);
         _performanceLogger.StopRecording();
-        _testRunning = false;
 
         // Save test results
         testData.frameData = _performanceLogger.getFpsLog();
